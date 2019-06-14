c06f2fc4083ea10347b668e5476a457bcf05fb9b
Ole Mussmann <ole@isbjorngames.com>
2019-06-14 22:20:27 +0200

    simplify setup

--- 03_cloud_services_hands-on/setup_lambda.sh
+++ 03_cloud_services_hands-on/setup_lambda.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y python3-pip unzip
-sudo snap install docker && sudo groupadd docker && sudo usermod -aG docker $USER
-pip3 install --user pipenv
-mkdir ~/lambda
-cd ~/lambda
-~/.local/bin/pipenv install --three awscli aws-sam-cli
-~/.local/bin/pipenv shell
+wget https://github.com/fastai/course-v3/raw/master/docs/production/aws-lambda.zip
+unzip aws-lambda.zip
+aws configure
